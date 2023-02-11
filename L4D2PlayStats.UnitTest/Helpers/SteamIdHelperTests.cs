ca49c68f4f7e5570ed4ab5590f9ed49a0f74fe3e
Altair Sossai <altairsossai@gmail.com>
2023-02-11 09:42:31 -0300

    Add tests to SteamIdHelper

---
+++ L4D2PlayStats.UnitTest/Helpers/SteamIdHelperTests.cs
@@ -0,0 +1,25 @@
+using FluentAssertions;
+using L4D2PlayStats.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace L4D2PlayStats.UnitTest.Helpers;
+
+[TestClass]
+public class SteamIdHelperTests
+{
+    [TestMethod]
+    public void SteamIdToCommunityId()
+    {
+        SteamIdHelper.SteamIdToCommunityId("STEAM_0:0:90628109").Should().Be(76561198141521946);
+        SteamIdHelper.SteamIdToCommunityId("STEAM_0:1:91233069").Should().Be(76561198142731867);
+        SteamIdHelper.SteamIdToCommunityId("STEAM_0:1:11181514").Should().Be(76561197982628757);
+    }
+
+    [TestMethod]
+    public void CommunityIdToSteam3()
+    {
+        SteamIdHelper.CommunityIdToSteam3(76561198141521946).Should().Be("[U:1:181256218]");
+        SteamIdHelper.CommunityIdToSteam3(76561198142731867).Should().Be("[U:1:182466139]");
+        SteamIdHelper.CommunityIdToSteam3(76561197982628757).Should().Be("[U:1:22363029]");
+    }
+}
